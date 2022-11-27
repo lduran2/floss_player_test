e2e5e1df4897c625d495d70c32935299f5709eee
Leomar Duran <29983534+lduran2@users.noreply.github.com>
2022-11-27 13:14:38 -0500

    [audlib] started test activity

---
+++ app/src/main/java/edu/temple/flossplayer/audlibtest/TestActivity.kt
@@ -0,0 +1,11 @@
+package edu.temple.flossplayer.audlibtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_audlib_test)
+    }
+}
