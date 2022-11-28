9d68ecb700b479ac861deae0d5e3820ee1945cc2
Leomar Duran <29983534+lduran2@users.noreply.github.com>
2022-11-27 21:04:51 -0500

    [audlib] fixed book control and status

--- app/src/main/java/edu/temple/flossplayer/audlibtest/BookControlFragment.kt
+++ app/src/main/java/edu/temple/flossplayer/audlibtest/BookControlFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -12,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [BookStatusFragment.newInstance] factory method to
+ * Use the [BookControlFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class BookStatusFragment : Fragment() {
+class BookControlFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -28,10 +29,12 @@ class BookStatusFragment : Fragment() {
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_status, container, false)
+        return inflater.inflate(R.layout.fragment_book_control, container, false)
     }
 
     companion object {
@@ -41,15 +44,16 @@ class BookStatusFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment PlayerControlFragment.
+         * @return A new instance of fragment BookControlFragment.
          */
         // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                BookStatusFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            BookControlFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
                 }
+            }
     }
 }
